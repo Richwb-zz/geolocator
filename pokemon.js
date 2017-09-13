1656ede6db0d2e55a451712e6fe49f89da84af83
unknown <richwbates@gmail.com>
2017-09-12 22:21:13 -0400

    created multi-dimensional array of types and locations they will appear

---
+++ pokemon.js
@@ -0,0 +1,19 @@
+typeLocation = ["bug","campground", "park", "zoo", "pet_store", "university", "veterinary_care"], 
+["dragon","library", "zoo", "stadium", "pet_store", "art_gallery", "veterinary_care"],
+["ice,","zoo", "night_club", "pet_store", "veterinary_care"],
+["fighting","gym", "zoo", "police", "pet_store", "fighting", "veterinary_care"],
+["fire","fire_station", "zoo", "bakery", "gas_station", "pet_store", "veterinary_care"],
+["flying","airport", "zoo", "travel_agency", "amusement_park", "pet_store", "veterinary_care"],
+["ground","campground","florist", "zoo", "pet_store", "veterinary_care"],
+["ghost","cemetery", "funeral_home", "zoo", "amusement_park", "pet_store", "veterinary_care", "movie_theater"],
+["ground","campground", "park", "zoo", "subway_station", "pet_store", "veterinary_care"],
+["electric","electrican", "electronics_store", "zoo", "train_station", "pet_store", "veterinary_care", "movie_theater"],
+["normal","park", "zoo", "resturant", "shopping mall", "school", "normal", "pet_store", "veterinary_care"],
+["poison","bar", "liquor_store", "zoo", "pharmacy", "pet_store", "rv_park", "veterinary_care"],
+["psychic","hospital", "zoo", "doctor", "pet_store", "university", "veterinary_care"],
+["rock","rock", "museum", "zoo", "cafe", "jewelry_store", "pet_store", "veterinary_care"],
+["water","aquarium", "zoo", "amusement_park", "pet_store", "veterinary_care"]];
+
+var testLocation = "subway_station";
+
+http://pokeapi.co/api/v2/
