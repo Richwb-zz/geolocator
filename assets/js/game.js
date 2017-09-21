165559b19cf9d9fe6c0e8de032c7270cae490d39
unknown <richwbates@gmail.com>
2017-09-21 17:00:41 -0400

    removed scroll bar

--- assets/js/game.js
+++ assets/js/game.js
@@ -63,7 +63,7 @@ $(document).on("click", "#battle-fled", function(){
 });
 
 $(document).on("click", ".view-pokedex", function(){
-	console.log("id " + $(this).find('span:first').text())
+	console.log("id " + $(this).find('span:first').text());
 	$("#pokedex-window").html("");
    $("#pokedex-window").load("pokedex.html");
 	
