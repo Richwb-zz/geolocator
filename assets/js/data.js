ab1e3d1d09148ab22677ae3031fc4acd146691e5
unknown <richwbates@gmail.com>
2017-09-20 20:58:40 -0400

    test7

--- assets/js/data.js
+++ assets/js/data.js
@@ -15,29 +15,6 @@ var provider = new firebase.auth.GoogleAuthProvider();
 var fdb = firebase.database();
 var user;
 
-firebase.auth().getRedirectResult().then(function(result) {
-  debugger;
-  if (result.credential) {
-    // This gives you a Google Access Token. You can use it to access the Google API.
-    var token = result.credential.accessToken;
-    // ...
-  }else{
-    firebase.auth().signInWithRedirect(provider);
-  }
-  // The signed-in user info.
-  user = result.user;
-}).catch(function(error) {
-  // Handle Errors here.
-  var errorCode = error.code;
-  var errorMessage = error.message;
-  // The email of the user's account used.
-  var email = error.email;
-  // The firebase.auth.AuthCredential type that was used.
-  var credential = error.credential;
-  // ...
-});
-
-
 function foundPokedex(pokeInfo){
   fdb.ref("User/pokedex/")
   .once("value")
--- assets/js/game.js
+++ assets/js/game.js
@@ -1,4 +1,26 @@
-
+console.log("new stuff5");
+firebase.auth().getRedirectResult().then(function(result) {
+  debugger;
+  if (result.credential) {
+    // This gives you a Google Access Token. You can use it to access the Google API.
+    var token = result.credential.accessToken;
+    // ...
+  }else{
+    firebase.auth().signInWithRedirect(provider);
+  }
+  // The signed-in user info.
+  user = result.user;
+  debugger;
+}).catch(function(error) {
+  // Handle Errors here.
+  var errorCode = error.code;
+  var errorMessage = error.message;
+  // The email of the user's account used.
+  var email = error.email;
+  // The firebase.auth.AuthCredential type that was used.
+  var credential = error.credential;
+  // ...
+});
 
 function pokeFound(pokeInfo){
 	pokeFoundInfo = pokeInfo;
